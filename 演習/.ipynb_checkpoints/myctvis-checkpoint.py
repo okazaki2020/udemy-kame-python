e9df9b73a31c229f5a8bd04751e8f109f662d45e
okazaki2020 <okazakimasaya@okazakiayanoAir.elecom>
2020-09-07 23:27:19 +0900

    finish 演習

--- 演習/.ipynb_checkpoints/myctvis-checkpoint.py
+++ 演習/.ipynb_checkpoints/myctvis-checkpoint.py
@@ -2,6 +2,7 @@ import pandas as pd
 from glob import glob
 import nibabel as nib
 import numpy as np
+from matplotlib import pyplot as plt
 
 def _get_df(base_url="../public-covid-data", folder="rp_im"):
     pathlist = glob("{}/{}/*".format(base_url,folder))
@@ -53,3 +54,47 @@ def overlay(gray_volume, mask_volume, mask_color, alpha=0.3):
     overlayed = np.where(mask_filter, gray_volume*(1-alpha) + mask_color*alpha, gray_volume).astype(np.uint8) 
     
     return overlayed
+
+#CT画像を並べて表示
+def vis_overlay(overlay,
+                original_volume,
+                mask_volume,
+                cols=4,display_num=20,figsize=(20,20)):
+    
+    rows = (display_num - 1) // cols + 1 
+    total_num = overlay.shape[-2]
+    fig, ax = plt.subplots(rows,cols,figsize=figsize)
+    interval = total_num / display_num
+    if interval < 1:
+        interval = 1
+        
+    for i in range(display_num):
+        row_i = i//cols
+        col_i = i%cols
+        idx = int(i * interval)
+        #統計値取得
+        stats = get_hu_stats(original_volume[:,:,idx],mask_volume[:,:,idx])
+        title = 'slice #: {}'.format(idx)
+        title += '\nggo mean:{:.0f}±{:.0f}'.format(stats['ggo_mean'], stats['ggo_std'])
+        title += '\nconsli mean:{:.0f}±{:.0f}'.format(stats['consolidation_mean'], stats['consolidation_std'])
+        title += '\neffusion mean:{:.0f}±{:.0f}'.format(stats['effusion_mean'], stats['effusion_std'])
+        #plot
+        ax[row_i,col_i].imshow(overlay[:,:,idx])
+        #plot情報を表示する
+        ax[row_i,col_i].set_title(title)
+        ax[row_i,col_i].axis("off")
+    fig.tight_layout()
+        
+        
+        
+#統計値計算
+def get_hu_stats(volume,mask_volume,
+                label_dict={1: "ggo",2: "consolidation", 3: "effusion"}):
+    result = {}
+    
+    for label in label_dict.keys():
+        prefix = label_dict[label]
+        roi_hu = volume[np.equal(mask_volume, label)]
+        result[prefix + "_mean"] = np.mean(roi_hu)
+        result[prefix + "_std"] = np.mean(roi_hu)
+    return result
